1beb8f59553d52e0ae265e75b3882da412509297
Jana Langfeldt <jana.langfeldt@bmw.de>
2021-07-17 12:54:49 +0200

    Changed the welcome message for the users

--- bikeshare.py
+++ bikeshare.py
@@ -10,7 +10,7 @@ DAYS = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sun
 
 def get_filters():
 
-    print('Hello! Let\'s explore some US bikeshare data!')
+    print('Hello! Do you want to explore Bikeshare data?')
     # TO DO: get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     while True:
         city = input('Please choose a city: Chicago, New York City or Washington: ').lower()
